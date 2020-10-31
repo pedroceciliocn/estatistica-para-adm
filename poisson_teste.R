6f20d828b3ecca1d12d5240eb4ccf2bcfec56ca4
Pedro Neto <pedroceciliocn@gmail.com>
2020-10-30 21:21:24 -0300

    ggplot call added

--- poisson_teste.R
+++ poisson_teste.R
@@ -20,19 +20,19 @@ ppois(1, lambda = 5, lower = FALSE) #PELO MENOS 2 CHAMADAS (OU O COMPLEMENTAR 1-
 
 plot(dpois(x = 0:5, lambda = 5), type = "b")
 #usando ggplot2
-ggplot(data.frame(x=c(0:5)), aes(x)) +
+ggplot2::ggplot(data.frame(x=c(0:5)), aes(x)) +
   geom_line(aes(y=dpois(x, 5)), colour="red") 
 
   
 #usando ggplot direito
 df_dpois_chamadas <- data.frame(n_de_chamadas_ph = 0:5, prob = dpois(x = 0:5, lambda = 5))
-ggplot(data = df_dpois_chamadas, mapping = aes(x = n_de_chamadas_ph, y = prob)) +
+ggplot2::ggplot(data = df_dpois_chamadas, mapping = aes(x = n_de_chamadas_ph, y = prob)) +
   geom_line() +
   geom_text(aes(label = round(prob,4), y = prob + 0.01),
             position = position_dodge(0.9),
             size = 3,
             vjust = 0) +
-  labs(title = "Distribuição de Poisson",
+  labs(title = "Distribui??o de Poisson",
        subtitle = "Chamadas: Probabilidade de 0 a 5 por hora num fim de semana.",
        x = "Sucessos (x)",
        y = "Probabilidade")
@@ -40,13 +40,13 @@ ggplot(data = df_dpois_chamadas, mapping = aes(x = n_de_chamadas_ph, y = prob)) 
 
 #### USANDO GGPLOT MAS COM BARRAS
 df_dpois_chamadas <- data.frame(n_de_chamadas_ph = 0:5, prob = dpois(x = 0:5, lambda = 5))
-ggplot(data = df_dpois_chamadas, mapping = aes(x = n_de_chamadas_ph, y = prob)) +
+ggplot2::ggplot(data = df_dpois_chamadas, mapping = aes(x = n_de_chamadas_ph, y = prob)) +
   geom_col() +
   geom_text(aes(label = round(prob,4), y = prob + 0.01),
             position = position_dodge(0.9),
             size = 3,
             vjust = 0) +
-  labs(title = "Distribuição de Poisson",
+  labs(title = "Distribui??o de Poisson",
        subtitle = "Chamadas: Probabilidade de 0 a 5 por hora num fim de semana.",
        x = "Sucessos (x)",
        y = "Probabilidade")
@@ -54,7 +54,7 @@ ggplot(data = df_dpois_chamadas, mapping = aes(x = n_de_chamadas_ph, y = prob)) 
   
 
 
-ggplot(transform(data.frame(x=c(0:5)), y=dpois(x, 5)), aes(x, y)) + 
+ggplot2::ggplot(transform(data.frame(x=c(0:5)), y=dpois(x, 5)), aes(x, y)) + 
   geom_bar(stat="identity")
 
 
@@ -67,7 +67,7 @@ events <- 0:5
 density <- dpois(x = events, lambda = 5)
 prob <- ppois(q = events, lambda = 5, lower.tail = TRUE)
 df_poisson <- data.frame(events, density, prob)
-ggplot(df_poisson, aes(x = factor(events), y = density)) +
+ggplot2::ggplot(df_poisson, aes(x = factor(events), y = density)) +
   geom_col() +
   geom_text(
     aes(label = round(density,2), y = density + 0.01),
